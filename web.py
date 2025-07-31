6b44ce00571f593849dff3ed3624586668345e14
Milon Hossen <milonhosain382@gmail.com>
2025-08-01 00:08:34 +0600

    Update web.py

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import json
+import os
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
@@ -19,4 +20,5 @@ def redirect_page(code):
         return "Invalid or expired link.", 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))  # Render থেকে PORT নিবে
+    app.run(host="0.0.0.0", port=port)
